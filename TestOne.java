1c8fe04c4ccb5ce3a168e595909e83eea3351f97
unknown <eva073192@yahoo.com>
2021-10-29 20:42:59 -0400

    added TestOne.java

---
+++ TestOne.java
@@ -0,0 +1,26 @@
+package dayOne;
+
+import java.sql.Driver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestOne { {
+	
+	//navigate to the URL
+	String url = "https://vax4nyc.nyc.gov/patient/s/";
+	
+	WebDriver driver = new FirefoxDriver();
+	
+	System.out.println("Welcome");
+    System.out.println("To");
+    System.out.println("GeeksforGeeks");
+	
+	
+	
+} }
+
+
+
